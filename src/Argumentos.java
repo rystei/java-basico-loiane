70ad6681abaa7ffd6406f0d7a5a0228b11c595af
Your Name <gustavo.telles@edu.unifil.br>
2026-01-21 21:26:29 -0300

    Aula args

---
+++ src/Argumentos.java
@@ -0,0 +1,9 @@
+public class Argumentos {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá mundo " + args[0]);
+
+
+    }
+}
---
+++ src/Argumentos2.java
@@ -0,0 +1,9 @@
+public class Argumentos2 {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá mundo " + args[0]);
+
+
+    }
+}
